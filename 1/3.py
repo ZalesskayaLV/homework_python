7fce01f07f6d346d8dd90695afdd50948a8add6e
Lyubov <zalesskayalyubov@gmail.com>
2022-07-10 00:35:18 +0400

    Formatted task 3, 4

--- 1/3.py
+++ 1/3.py
@@ -1,6 +1,6 @@
 # Напишите программу, которая принимает на вход
 #  координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
-#  и выдаёт номер четверти плоскости, 
+#  и выдаёт номер четверти плоскости,
 # в которой находится эта точка (или на какой оси она находится).
 
 # *Пример:*
@@ -14,36 +14,29 @@
 #         try:
 #             number = int(message)
 #             return number
-                
-        
+
+
 #         except:
 #             print("error, you didn't enter a number!")
 #             break
 
 
 def quarter_search(x, y):
-    if x>0 and y>0:
+    if x > 0 and y > 0:
         return 1
-    elif x>0 and y<0:
+    elif x > 0 and y < 0:
         return 4
-    elif x<0 and y>0:
+    elif x < 0 and y > 0:
         return 2
-    elif x<0 and y<0:
+    elif x < 0 and y < 0:
         return 3
-
 
 
 x = int(input('enter coordinate x: '))
 y = int(input('enter coordinate y: '))
 
-if x==0 or y==0:
+if x == 0 or y == 0:
     print('enter coordinates different from zero')
 else:
     result = quarter_search(x, y)
     print(f'coordinates are in {result} quarter')
-
-
-
-
-    
-
--- 1/4.py
+++ 1/4.py
@@ -6,20 +6,18 @@
 
 quater = int(input('enter quater number: '))
 
-while quater in range (1, 5):
+while quater in range(1, 5):
     if quater == 1:
-         print('x > 0, y > 0')
-         break
+        print(' x > 0, y > 0 ')
+        break
     elif quater == 2:
-        print ('x<0, y>0')
+        print(' x < 0, y > 0')
         break
     elif quater == 3:
-        print ('x<0, y<0')
+        print(' x < 0, y < 0 ')
         break
     elif quater == 4:
-        print ('x > 0, y < 0')
+        print(' x > 0, y < 0 ')
         break
 else:
     print('just four quarters in Cartesian coordinate system')
-
-
