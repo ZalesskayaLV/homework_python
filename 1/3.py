8ccc2cedc039d6b2c9a26b91136e3d09aaa99af8
Lyubov <zalesskayalyubov@gmail.com>
2022-07-09 23:52:33 +0400

    Add task 3 complete

--- 1/3.py
+++ 1/3.py
@@ -9,3 +9,41 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
+# def input_number(message):  (DO NOT WORK! NEED TO THINK MORE)
+#     while True:
+#         try:
+#             number = int(message)
+#             return number
+                
+        
+#         except:
+#             print("error, you didn't enter a number!")
+#             break
+
+
+def quarter_search(x, y):
+    if x>0 and y>0:
+        return 1
+    elif x>0 and y<0:
+        return 4
+    elif x<0 and y>0:
+        return 2
+    elif x<0 and y<0:
+        return 3
+
+
+
+x = int(input('enter coordinate x: '))
+y = int(input('enter coordinate y: '))
+
+if x==0 or y==0:
+    print('enter coordinates different from zero')
+else:
+    result = quarter_search(x, y)
+    print(f'coordinates are in {result} quarter')
+
+
+
+
+    
+
