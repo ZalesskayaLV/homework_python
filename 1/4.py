f871d0831d0badbaacaf88fcfa7a140e6f43df45
Lyubov <zalesskayalyubov@gmail.com>
2022-07-10 00:27:32 +0400

    Add task 4

---
+++ 1/4.py
@@ -0,0 +1,25 @@
+# 4-Напишите программу, которая
+#  по заданному номеру четверти,
+#  показывает диапазон возможных координат
+#  точек в этой четверти(x и y).
+
+
+quater = int(input('enter quater number: '))
+
+while quater in range (1, 5):
+    if quater == 1:
+         print('x > 0, y > 0')
+         break
+    elif quater == 2:
+        print ('x<0, y>0')
+        break
+    elif quater == 3:
+        print ('x<0, y<0')
+        break
+    elif quater == 4:
+        print ('x > 0, y < 0')
+        break
+else:
+    print('just four quarters in Cartesian coordinate system')
+
+
