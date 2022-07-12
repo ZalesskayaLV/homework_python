7989a52a815888fb84aad622cbdce7538e73073d
Lyubov <zalesskayalyubov@gmail.com>
2022-07-12 14:17:32 +0400

    add code edits in 1 task

--- 1/1.py
+++ 1/1.py
@@ -6,19 +6,20 @@
 # - 7 -> да
 # - 1 -> нет
 
-def input_number(message):
+def input_number(text):
     while True:
+        message = input(text)
         try:
             number = int(message)
             return number
         
         except:
             print("error, you didn't enter a number!")
-            break
+            
 
 
 
-day_of_week = input_number(input('Введите цифру от 1 до 7, обозначающую день недели: '))
+day_of_week = input_number('Введите цифру от 1 до 7, обозначающую день недели: ')
 
 if day_of_week in range(1, 6):
     print("нет")
